2020aa0755e4b0c0a1779d428b3b5cb4c4216500
zhangxuanwei <zhangxuanwei@sensetime.com>
2021-09-25 22:14:13 +0800

    ✨事件并发服务

---
+++ 9.25/ch8/clock2.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn) // handle one connection at a time
+	}
+}
