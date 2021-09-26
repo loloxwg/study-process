6aa2068a266021e432fce8a47659e8b0725b70e6
zhangxuanwei <zhangxuanwei@sensetime.com>
2021-09-26 17:43:02 +0800

    ✨管道和协程协同工作的案例1

---
+++ 9.26/chanAndGoroutine/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	intChan := make(chan int, 10)
+	exitChan := make(chan bool, 1)
+
+	go writeData(intChan)
+
+	go readData(intChan, exitChan)
+
+	for {
+		_, ok := <-exitChan
+		if !ok {
+			break
+		}
+	}
+
+}
+
+func writeData(intChan chan int) {
+	for i := 0; i < 50; i++ {
+		intChan <- i
+		fmt.Println("writeData: ", i)
+	}
+	close(intChan)
+}
+
+func readData(intChan chan int, exitChan chan bool) {
+	for {
+		v, ok := <-intChan
+		if !ok {
+			break
+		}
+		fmt.Println("readData: ", v)
+	}
+	exitChan <- true
+	close(exitChan)
+}
