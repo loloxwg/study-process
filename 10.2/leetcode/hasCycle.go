c77861375d8682a1fd26c8ac8ef10d55949b7c8b
loloxwg <loloxwg@outlook.com>
2021-10-03 17:36:29 +0800

    🚀 判断有无环

---
+++ 10.2/leetcode/hasCycle.go
@@ -0,0 +1,20 @@
+package main
+
+
+func hasCycle(head *ListNode) bool {
+	seen := map[*ListNode]struct{}{}
+
+	for head != nil {
+		if _, ok := seen[head]; ok {
+			return true
+		}
+		seen[head] = struct{}{}
+		head = head.Next
+	}
+	return false
+}
+func main() {
+	var head *ListNode
+	hasCycle(head)
+
+}
