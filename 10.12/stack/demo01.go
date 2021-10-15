354c8affe9a716d0eaa092b238cf51791689b4da
loloxwg <loloxwg@outlook.com>
2021-10-15 22:25:02 +0800

    🚀 栈，负载均衡

---
+++ 10.12/stack/demo01.go
@@ -0,0 +1,25 @@
+package main
+
+
+func main() {
+	mul(3,4)
+}
+func mul(a ,b int)int {
+	return a*b
+}
+//
+//class Solution{
+//public void merge(int a [], int m, int b [], int n){
+//int i = m-1, j = n-1, k = m+n-1;
+//while(i>=0 && j>=0){
+//if(a[i] > b[j])
+//a[k--] =a[i--];
+//else
+//a[k--] = b[j--];
+//}
+//while(j>=0){
+//nums[k--] = nums[j--];
+//}
+//}
+//}
+//
